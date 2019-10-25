89ac93267e50bd9cb36850da4dd9cffc6e97d795
JamesMutahi <mutahijames0@gmail.com>
2019-10-25 23:59:25 +0300

    Add ImageField and reviews model

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
+    image = models.ImageField(blank=True, null=True)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -15,3 +16,11 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
+
+
+class Reviews(models.Model):
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="reviews")
+    posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.TextField(max_length=100)
+    date_posted = models.DateTimeField(default=timezone.now)
+    clap = models.IntegerField(blank=True, default=0)
